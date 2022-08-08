db55a2174cac4ae25bdd4cb8f91120258a29f7b0
ysyss <alsdbtjd0103@naver.com>
2022-08-08 20:54:35 +0900

    재정리

--- Client/util/auth.js
+++ Client/util/auth.js
@@ -1,12 +1,10 @@
 import axios from "axios";
 
-//token 발급 함수
 export async function login(email, password) {
   const url = `http://localhost:3000/auth/login`;
   const response = await axios.post(url, {
     email: email,
     password: password,
   });
-  const token = response.data.token;
-  return token;
+  return response.data;
 }
